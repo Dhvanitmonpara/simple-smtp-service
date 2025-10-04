57f789e0f9f92273917287a62257a22ae5f598e2
Dhvanitmonpara <monparadhvanit@gmail.com>
2025-10-04 12:02:40 +0530

    removed vercel/node

--- api/index.ts
+++
@@ -1,7 +0,0 @@
-import { env } from "../src/conf/env.js";
-import app from "../src/app.js";
-import { VercelRequest, VercelResponse } from "@vercel/node";
-
-export default (req: VercelRequest, res: VercelResponse) => {
-  return app(req, res)
-}
