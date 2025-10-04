f473463c0738e6334ac1a9480f17fa2a00eac7c2
Dhvanitmonpara <monparadhvanit@gmail.com>
2025-10-04 11:30:27 +0530

    fixed tsconfig

--- api/index.ts
+++ api/index.ts
@@ -2,8 +2,6 @@ import { env } from "../src/conf/env.js";
 import app from "../src/app.js";
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
-const port = env.PORT || 8000;
-
 export default (req: VercelRequest, res: VercelResponse) => {
   return app(req, res)
 }
